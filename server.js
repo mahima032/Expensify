c8091bfe75b9355f1f8a31e8cd0a3fb4dbfb2464
mahima032 <21CS032@charusat.edu.in>
2023-10-19 00:00:32 +0530

    ch

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors =require('cors')
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
-const path=require('path');
+const path = require('path');
 const connectDb = require('./config/connectDb');
 const app = express();
 
@@ -22,9 +22,10 @@ app.use(cors());
 app.use('/api/v1/users',require('./routes/userRoutes'))
 //transaction routes
 app.use('/api/v1/transaction',require("./routes/transactionRoute"));
-app.use(express.static(path.join(__dirname,'./client/build')));
+app.use(express.static(path.join(__dirname,'../client/build')));
+
 app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
 })
 //port
 const PORT = 8080 || process.env.PORT;
